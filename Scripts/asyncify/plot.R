9c0d2a6108ddf3cf20ffd48f1b9ce56e8c4831d6
Donald Pinckney <donald_pinckney@icloud.com>
2020-03-24 23:05:42 -0400

    Plotting!

--- Scripts/asyncify/plot.R
+++ Scripts/asyncify/plot.R
@@ -2,6 +2,7 @@
 
 library(ggplot2)
 library(reshape2)
+library(scales)
 
 load('Results/asyncify/results.rda') # loads it to a variable data
 
@@ -19,6 +20,11 @@ p <- ggplot(mean_data, aes(y=Time, x=yield, group=Implementation)) +
   geom_point(aes(color=Implementation)) +
   # geom_errorbar(aes(ymin=Time-sd, ymax=Time+sd), width=.1) +
   labs(y="Time (s)", x="Iterations/yield") + 
+  ylim(0, 25) +
+  scale_x_continuous(trans='log2', 
+                    breaks = trans_breaks("log2", function(x) 2^x),
+                    labels = trans_format("log2", math_format(2^.x))
+                    ) + 
   ggtitle('User-threaded approximation of pi')
 
 ggsave("Results/asyncify/plot.pdf", p)
