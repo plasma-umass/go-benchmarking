16fb30419d8ccdae492b4c2580cece21a1a2006d
Donald Pinckney <donald_pinckney@icloud.com>
2020-05-22 11:32:34 -0400

    Loop in build script

--- Scripts/uthreads/build_all.sh
+++ Scripts/uthreads/build_all.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-Scripts/uthreads/build_thread_test.sh CONTS programs/uthreads/src/pi.c programs/uthreads/wasm/pi
-Scripts/uthreads/build_thread_test.sh SWAPCONTEXT programs/uthreads/src/pi.c programs/uthreads/native/pi
+rm -f programs/uthreads/wasm/*
+rm -f programs/uthreads/native/*
+
+for p in programs/uthreads/src/*.c; do
+    f=$(basename "$p" .c)
+    Scripts/uthreads/build_thread_test.sh CONTS "programs/uthreads/src/$f.c" "programs/uthreads/wasm/$f"
+    Scripts/uthreads/build_thread_test.sh SWAPCONTEXT "programs/uthreads/src/$f.c" "programs/uthreads/native/$f"
+done
