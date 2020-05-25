143dfbfee456c0be5a5c49df4197c49566018700
Donald Pinckney <donald_pinckney@icloud.com>
2020-05-24 22:35:51 -0400

    Fix build script

--- Scripts/uthreads/build_all.sh
+++ Scripts/uthreads/build_all.sh
@@ -4,6 +4,10 @@ rm -f programs/uthreads/CONTS/*
 rm -f programs/uthreads/SWAPCONTEXT/*
 rm -f programs/uthreads/PTHREAD/*
 
+mkdir -p programs/uthreads/CONTS/
+mkdir -p programs/uthreads/SWAPCONTEXT/
+mkdir -p programs/uthreads/PTHREAD/
+
 for p in programs/uthreads/src/*.c; do
     f=$(basename "$p" .c)
     Scripts/uthreads/build_thread_test.sh CONTS "programs/uthreads/src/$f.c" "programs/uthreads/CONTS/$f"
