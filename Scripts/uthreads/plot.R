2e035cec363fc10f9db1624aafd9cb6eeb83eedb
Donald Pinckney <donald_pinckney@icloud.com>
2020-05-24 22:33:02 -0400

    Update plotting code

--- Scripts/uthreads/plot.R
+++ Scripts/uthreads/plot.R
@@ -1,9 +1,13 @@
+#!/usr/bin/env Rscript
+
 require(dplyr)
 require(ggplot2)
 load("Results/uthreads/results.rda")
 
-df %>% ggplot(aes(x=test, fill=impl, y=time_mean)) +
+p <- df %>% ggplot(aes(x=test, fill=impl, y=time_mean)) +
   geom_col(position=position_dodge()) +
   geom_errorbar(aes(ymin=time_mean-time_sd, ymax=time_mean+time_sd), 
                 width=.1, position=position_dodge(.9)) +
-  labs(y="Time (s)", x="Benchmark")
+  labs(y="Time (s)", x="Benchmark");
+
+ggsave("Results/uthreads/uthreads.pdf", p)
