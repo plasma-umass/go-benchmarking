f7a15470d07fb7e7f7861127d6927c74d65aea52
Donald Pinckney <donald_pinckney@icloud.com>
2020-05-25 21:40:09 +0000

    Plots

--- Scripts/uthreads/plot.R
+++ Scripts/uthreads/plot.R
@@ -2,12 +2,14 @@
 
 require(dplyr)
 require(ggplot2)
+
 load("Results/uthreads/results.rda")
 
-p <- df %>% ggplot(aes(x=test, fill=impl, y=time_mean)) +
+p <- df %>% filter(!(impl %in% c("NATIVE_SWAPCONTEXT"))) %>% ggplot(aes(x=test, fill=impl, y=time_mean)) +
   geom_col(position=position_dodge()) +
   geom_errorbar(aes(ymin=time_mean-time_sd, ymax=time_mean+time_sd), 
                 width=.1, position=position_dodge(.9)) +
-  labs(y="Time (s)", x="Benchmark");
+  labs(y="Time (s)", x="Benchmark") +
+  facet_wrap(~terms_per_yield_log2, ncol=1);
 
 ggsave("Results/uthreads/uthreads.pdf", p)
