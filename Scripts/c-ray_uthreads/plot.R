d617b6b5b402173d8a652050139d4b4d37ad0b11
Donald Pinckney <donald_pinckney@icloud.com>
2020-07-06 17:18:50 -0400

    Work more on plotss

--- Scripts/c-ray_uthreads/plot.R
+++ Scripts/c-ray_uthreads/plot.R
@@ -3,17 +3,26 @@
 require(dplyr)
 require(ggplot2)
 require(scales)
+require(stringr)
+
+source("Scripts/c-ray_uthreads/theme.R")
 
 gmean <- function(x) { exp(mean(log(x))) }
 combine_sd <- function(x) { sqrt(sum(x*x)) }
 
 load("Results/c-ray_uthreads/results.rda")
 
-# Remove stormtroopers since we accidentally used 1 thread only
+# Remove stormtroopers since it crashed Asyncify
 df <- df %>% filter(test != "input/stormtroopers.json")
 
 # Rename things
 df <- df %>% mutate(impl=recode(impl, serial="Serial", conts="Wasm/k", asyncify="Asyncify"))
+
+## Need to do all this nonsense since R really sucks sometimes.
+new_level_mapping <- df$test %>% unique()
+new_level_mapping <- new_level_mapping %>% str_replace("input/", "") %>% str_replace(".json", "")
+names(new_level_mapping) <- df$test %>% unique()
+df <- df %>% mutate(test=recode(test, !!!new_level_mapping))
 
 # Compute baseline mean
 df_baseline <- df %>% filter(impl=="Serial") %>% group_by(test) %>% summarize(baseline=mean(time_mean))
@@ -30,29 +39,27 @@ df <- df %>% group_by(test) %>% mutate(baseline_relative_mean=time_mean / get_ba
 df <- df %>% filter(impl != "Serial")
 
 # Bar plot yield_every
-bar_yield_every <- 1024
+bar_yield_every <- 262144 # ~ 26 ms / yield
 df_bar <- df %>% filter(yield_every == bar_yield_every)
 df_bar <- df_bar %>% select(-c(yield_every,time_mean,time_sd))
 df_bar_gmean <- df_bar %>% group_by(impl) %>% summarize(baseline_relative_sd=combine_sd(baseline_relative_sd), test="Geom Mean", baseline_relative_mean = gmean(baseline_relative_mean))
 asyncify_1024_slowdown <- df_bar_gmean %>% filter(impl=="Asyncify") %>% select(baseline_relative_mean) /
   df_bar_gmean %>% filter(impl=="Wasm/k") %>% select(baseline_relative_mean)
-print(paste("At a yield rate of 1024, Asyncify is ", asyncify_1024_slowdown, "x slower than Wasm/k"))
+print(paste("At a yield rate of 262144, Asyncify is ", asyncify_1024_slowdown, "x slower than Wasm/k"))
 df_bar <- bind_rows(df_bar, df_bar_gmean)
+df_bar$test <- factor(df_bar$test, levels = c("fence", "hdr", "refraction", "scene", "venus", "Geom Mean"))
 p <- df_bar %>% ggplot(aes(x=test, fill=impl, y=baseline_relative_mean)) +
   geom_col(position=position_dodge()) +
-  geom_errorbar(aes(ymin=baseline_relative_mean-baseline_relative_sd, ymax=baseline_relative_mean+baseline_relative_sd), 
+  geom_errorbar(aes(ymin=baseline_relative_mean-2*baseline_relative_sd, ymax=baseline_relative_mean+2*baseline_relative_sd), 
                 width=.1, position=position_dodge(.9)) +
   labs(y="Relative Time (Serial = 1.0)", x="Benchmark", fill="Implementation") +
   geom_hline(yintercept=1) +
-  theme_bw() +
-  theme(
-    axis.text.x = element_text(angle = 30, hjust=1),
-    legend.position = "top") +
-  scale_fill_manual(values=c("#ff9966", "#6699ff")) +
   ylim(0, 1.5)
+p <- my_theme(p)
 print(p)
+my_save("Results/c-ray_uthreads/c-ray_uthreads_scenes.pdf", p)
 
-ggsave("Results/c-ray_uthreads/c-ray_uthreads_scenes.pdf", width=3.5, height=4)
+
 
 df_line <- df %>% group_by(impl, yield_every) %>% summarize(
   baseline_relative_sd=combine_sd(baseline_relative_sd), 
@@ -78,12 +85,8 @@ p <- df_line %>% ggplot(aes(
     labels = trans_format("log2", math_format(2^.x)),
     breaks = ticks) +
     # breaks = trans_breaks("log2", function(x) 2^x)) +
-  ylim(0, 3.5) +
-  theme_bw() +
-  theme(legend.position = "top") +
-  scale_fill_manual(values=c("#ff9966", "#6699ff")) +
-  scale_color_manual(values=c("#ff9966", "#6699ff"))
-
+  ylim(0, 3.5)
+p <- my_theme(p)
 print(p)
 
-ggsave("Results/c-ray_uthreads/c-ray_uthreads.pdf", p, width=3.5, height=4)
+my_save("Results/c-ray_uthreads/c-ray_uthreads.pdf", p)
---
+++ Scripts/c-ray_uthreads/theme.R
@@ -0,0 +1,23 @@
+require(ggplot2)
+require(scales)
+require(extrafont)
+loadfonts()
+
+my_theme <- function(p) {
+    return(p + 
+        theme_bw() + 
+        theme(
+            text = element_text(family = "CMU Serif", size=10),
+            legend.title = element_blank(),
+            legend.position = "top"
+        ) +
+        # theme(legend.position = "top") +
+        scale_fill_manual(values=c("#ff9966", "#6699ff")) +
+        scale_color_manual(values=c("#ff9966", "#6699ff"))
+    )
+}
+
+my_save <- function(name, p) {
+    ggsave(name, width=3.5, height=4)
+    embed_fonts(name)
+}
